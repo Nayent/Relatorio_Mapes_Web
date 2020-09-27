45a44306877acb88f9045613f76e34eb8a1996c1
Guilherme Nomura <nomura@pop-os.localdomain>
2020-09-27 19:42:34 -0300

    finished8

--- busca_consultas/admin.py
+++ busca_consultas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Consulta, Exame
+from .models import Consulta, Exame, Relatorio
 
 class ListandoConsultas(admin.ModelAdmin):
     list_display = ('numero_guia','nome_medico')
@@ -7,5 +7,9 @@ class ListandoConsultas(admin.ModelAdmin):
 class ListandoExame(admin.ModelAdmin):
     list_display = ('id', 'numero_guia_consulta','exame')
 
+class ListandoRelatorio(admin.ModelAdmin):
+    list_display = ('nome_medico')
+
 admin.site.register(Consulta, ListandoConsultas)
 admin.site.register(Exame, ListandoExame)
+admin.site.register(Relatorio, ListandoRelatorio)
